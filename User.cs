47af18add0898fd2b45b3688fcb814cbb42d7aea
Travis Park <travispark@Traviss-MacBook-Pro.local>
2022-01-19 13:58:08 -0800

    Added DateCreated to User class

--- User.cs
+++ User.cs
@@ -5,12 +5,14 @@
         public string Username;
         public string Password;
         public string PFPLocation;
+        public DateTime DateCreated;
 
-        public User(string username, string password, string pfpLocation)
+        public User(string username, string password, string pfpLocation, DateTime dateCreated)
         {
             Username = username;
             Password = password;
             PFPLocation = pfpLocation;
+            DateCreated = dateCreated;
         }
     }
 }
