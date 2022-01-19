3f0e6053ee9fa9536ebfe10624173fb25406b658
Travis Park <travispk268@gmail.com>
2022-01-19 14:11:50 -0800

    same here

--- User.cs
+++ User.cs
@@ -4,14 +4,6 @@
     {
         public string Username;
         public string Password;
-        public string PfpLocation;
-
-        public User(string username, string password, string pfpLocation)
-        {
-            Username = username;
-            Password = password;
-            PfpLocation = pfpLocation;
-        }
         public string PFPLocation;
         public DateTime DateCreated;
 
