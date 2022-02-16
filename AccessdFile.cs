b9c8ae4a6afe71adc8bc99178ef062a8b8177d39
Travis Park <travispk268@gmail.com>
2022-02-16 01:06:11 -0800

    Aparently, my constructors were too good for the "JsonConvert" thingey

--- AccessdFile.cs
+++ AccessdFile.cs
@@ -5,16 +5,18 @@
         public long UnixTime { get; private set; }
         public string Action { get; private set; }
 
-        public AccessdFile(string action)
+        public AccessdFile(string action, long unixTime = 0)
         {
-            UnixTime = DateTimeOffset.Now.ToUnixTimeSeconds();
+            if (unixTime == 0)
+            {
+                UnixTime = DateTimeOffset.Now.ToUnixTimeSeconds();
+            }
+            else
+            {
+                UnixTime = unixTime;
+            }
             Action = action;
         }
 
-        public AccessdFile(string action, long unixTime)
-        {
-            UnixTime = unixTime;
-            Action = action;
-        }
     }
 }
